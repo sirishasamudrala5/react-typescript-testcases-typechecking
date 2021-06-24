1671ade0b72b0137aa5bebdcbc41a4b1975cb065
sirisha samudrala <sirisha.samudrala5@gmail.com>
2021-06-25 01:56:43 +0530

    layoutcontainer testcases

--- src/components/LayoutContainer.test.tsx
+++ src/components/LayoutContainer.test.tsx
@@ -5,6 +5,7 @@ import LayoutContainer from './LayoutContainer';
 
 
 it('can browse to the about page', async () => {
+  // For `LayoutContainer`, you should be able to render the layout container, followed by navigating to the About page.
   render(<LayoutContainer />);
   const aboutLink = screen.getByTestId(`aboutLink`)
   const spy = jest.spyOn(aboutLink, 'click');
